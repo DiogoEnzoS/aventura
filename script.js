f213f7b4402357cdc80c1b8f1be6e4ef2dc06776
DiogoStoso <165948768+DiogoEnzoS@users.noreply.github.com>
2024-08-30 11:58:47 -0300

    upolonde

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const avance = document.querySelectorall( '.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
-avance. forEach( button =>  {
-    button.addeventlistener('click' function(){
-        const atual = document.querySelector('.ativo')
-        const proximoPasso = 'passo-' + this.getAttribute ('data-proximo')
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-        atual.classList.remove ('ativo')
-        document.getElementById ('proximopasso')
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
-
-} )
+})
